cdec44e748ed6e72894d8a19e55487527c9c6f75
imightknoiw <83017202+imightknoiw@users.noreply.github.com>
2022-09-13 11:47:25 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+var intvl1, intvl2;
+
+function genTerm(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+function search(){
+  var x = 0, y = 0, w=200, h=200;
+  var term = genTerm(Math.floor(Math.random()));
+  var win = window.open('https://www.bing.com/search?q=${term}', "", 
+          "width=" + w + ",height=" + h);
+  win.moveTo(x,y);
+}
+
+function start(){
+  setInterval(search, 4500);
+}
+
+function stop(){
+  clearInterval(intvl1);
+  clearInterval(intvl2);
+}
