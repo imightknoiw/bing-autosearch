231c67372ad818d23985a2303de8c7b7e4696037
imightknoiw <83017202+imightknoiw@users.noreply.github.com>
2022-09-13 18:22:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,17 +32,15 @@ function search() {
         stop();
     } else {
         var x = 582,
-            y = 350,
+            y = 0,
             w = 205,
             h = 200;
         var term = Math.random().toString(16).substr(2, 8);
         if (searchCt == 1) {
-            win = window.open(`https://www.bing.com/search?q=${term}`, "", "width=" + w + ",height=" + h);
+            win = window.open(`https://www.bing.com/search?q=${term}`, "", "width=" + w + ",height=" + h+",status=no,left=200");
             win.moveTo(x, y);
-            win.scroll(200, 0);
         } else if (searchCt > 1) {
             win.location.href = `https://www.bing.com/search?q=${term}`;
-            win.scroll(200, 0);
         }
         document.getElementById("searchCt").innerHTML = searchCt;
     }
