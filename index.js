5ecbe00a486079964a8b93261bd00af261ac7bd8
imightknoiw <83017202+imightknoiw@users.noreply.github.com>
2022-09-14 07:12:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function search() {
         stop();
     } else {
         var x = 582,
-            y = 500,
+            y = 400,
             w = 205,
             h = 200;
         var term = Math.random().toString(16).substr(2, 8);
