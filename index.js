6a1274456c95e8b358d3815a90867415a3f25458
v <83017202+imightknoiw@users.noreply.github.com>
2022-09-20 07:49:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var intvl1;
+var win = null;
 var active = false;
 var searchCt = 0;
-var win, searchGoal;
+var searchGoal;
 function randomStr(){
 let str = Math.random().toString(16).substr(2, 8);
 return str;
@@ -20,7 +20,6 @@ function toggleElems() {
 }
 
 function stop() {
-    clearInterval(intvl);
     searchCt = 0;
     win.close();
     win = null;
@@ -34,14 +33,14 @@ function search() {
     searchCt++;
     if (searchCt > searchGoal) {
         stop();
-    } else if (searchCt < document.getElementById("searchNum").value) {
+    } else if (searchCt < searchGoal) {
             win.location.href = `https://www.bing.com/search?q=${randomStr()}#id_rc`;
             waitLoad();
         }
         document.getElementById("searchCt").innerHTML = searchCt;
 }
 function waitLoad(){
-    win.addEventListener('load', function(){setTimeout(search, 3000)}, true);
+    win.addEventListener('load', function(){setTimeout(search, 3000)});
 }
 function start() {
     searchGoal = document.getElementById("searchNum").value; 
@@ -54,4 +53,13 @@ function start() {
     active = true;
     document.getElementById("stopBtn").style.display = "block";
     toggleElems();
+}
+var child = window.open('http://google.com','','toolbar=0,status=0,width=626,height=436');
+var timer = setInterval(checkChild, 500);
+
+function checkClose() {
+    if (win.closed) {
+        alert("Child window closed");   
+        clearInterval(timer);
+    }
 }
