11928502e487a8772fe2e1a6a5f3c867ef758447
v <83017202+imightknoiw@users.noreply.github.com>
2022-09-20 07:30:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var intvl1;
 var active = false;
 var searchCt = 0;
 var win;
-
+function randomStr(){
+let str = Math.random().toString(16).substr(2, 8);
+return str;
+}
 function toggleElems() {
     var elems = document.getElementsByClassName("hide");
     if (active == true) {
@@ -28,27 +31,24 @@ function stop() {
 }
 
 function search() {
-    searchCt++;
     if (searchCt > document.getElementById("searchNum").value) {
         stop();
-    } else {
-        var x = 582,
-            y = 335,
-            w = 205,
-            h = 200;
-        var term = Math.random().toString(16).substr(2, 8);
-        if (searchCt == 1) {
-            win = window.open(`https://www.bing.com/search?q=${term}#id_rc`, "", "width=" + w + ",height=" + h + ",status=no,left=200");
-            win.moveTo(x, y);
-        } else if (searchCt > 1) {
-            win.location.href = `https://www.bing.com/search?q=${term}#id_rc`;
+    } else if (searchCt > 0) {
+            searchCt++;
+            win.location.href = `https://www.bing.com/search?q=${randomStr()}#id_rc`;
+            waitLoad();
         }
         document.getElementById("searchCt").innerHTML = searchCt;
-    }
 }
-
+function waitLoad(){
+    win.addEventListener('load', function(){setTimeout(search, 3000)}, false);
+}
 function start() {
-    intvl = setInterval(search, 4000);
+    var x = 582,y = 335,w = 205,h = 200;
+    win = window.open(`https://www.bing.com/search?q=${randomStr()}#id_rc`, "", "width=" + w + ",height=" + h + ",status=no");
+    win.moveTo(x, y);
+    searchCt++;
+    waitLoad();
     active = true;
     document.getElementById("stopBtn").style.display = "block";
     toggleElems();
